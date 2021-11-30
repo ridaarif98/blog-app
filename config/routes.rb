2450dccc83c215f1ae3f3eeb816e02e75da91193
ridaarif98 <ridaarif16@gmail.com>
2021-11-30 13:51:54 +0100

    Create controller, action methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/show'
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Nested routes allow you to capture this relationship in your routing
 
