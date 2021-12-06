0ca73a9552fe5aea15f70ec7388513dbdfa9ae10
ridaarif98 <ridaarif16@gmail.com>
2021-12-06 11:40:19 +0100

    Create user model and set its assciations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     # Removes one or more objects from the collection by setting their foreign keys to NULL. 
     # Objects will be in addition destroyed if they're associated with dependent: :destroy
-    belongs_to :posts, dependent: :destroy
-    belongs_to :comments, dependent: :destroy
-    belongs_to :likes, dependent: :destroy
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
 
     def recent_posts
-        User.last(3)
+        posts.last(3)
     end
 end
