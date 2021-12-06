c0dafa1599d7bcaedec3cf8a76fb1713d570cf2a
ridaarif98 <ridaarif16@gmail.com>
2021-12-06 12:06:52 +0100

    Add method for recent comments and post counter in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    # A method that updates the posts counter for a user.
+    after_create :update_post_counter
+
+    def update_post_counter
+        user.update(user.posts_counter: user.posts.size)
+    end
+
+    def recent_comments
+        comments.last(5)
+    end
 end
