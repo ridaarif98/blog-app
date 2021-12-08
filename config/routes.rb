5bf863afb4677223da098a81444eeb4250a7b6ec
ridaarif98 <ridaarif16@gmail.com>
2021-12-08 14:28:09 +0100

    Create form to add likes and its controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :comments, only: [:create] 
+    resources :comments, only: [:create]
+    resources :likes, only: [:create] 
   end
 end
