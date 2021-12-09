c26451a595d3361b82ef3e3dfe3d99260e5bde54
ridaarif98 <ridaarif16@gmail.com>
2021-12-09 12:03:28 +0100

    Add validations for post and user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  validates :title, presence: true, length: { maximun: 250 }
+  validates :comments_counter,numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   # Registers a callback to be called after a record is created
   after_create :update_post_counter
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true  ,greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
   def recent_posts
     posts.last(3)
   end
