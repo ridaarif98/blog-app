95c07781d5d3d766107ecd238a7b26ce7c12815f
ridaarif98 <ridaarif16@gmail.com>
2021-12-09 15:10:09 +0100

    Fix linter errors

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'Update like counter' do
+    let(:user) { User.create(name: 'Rida') }
+    let(:post) { Post.create(title: 'Test', text: 'This is test post', user_id: user.id) }
+    let(:comment) { Comment.create(user_id: user.id, post_id: post.id) }
 
-    describe 'Update like counter' do
-        let(:user) {User.create(name: 'Rida')}
-        let(:post) {Post.create(title: 'Test', text:'This is test post', user_id: user.id)}
-        let(:comment) { Comment.create(user_id: user.id, post_id: post.id) }
-   
-        it 'should like counter for post' do
-           post.comments.create(user: user)
-           expect(post.comments_counter).to eq 1
-        end
-   
-        it 'should like counter for post' do 
-           expect(comment.update_comment_counter).to eq true
-        end
+    it 'should like counter for post' do
+      post.comments.create(user: user)
+      expect(post.comments_counter).to eq 1
     end
+
+    it 'should like counter for post' do
+      expect(comment.update_comment_counter).to eq true
+    end
+  end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,47 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject {User.new(name:"Ali", photo: "https://www.qttoyslondon.com/wp-content/uploads/2021/01/pokemon-45inch-battle-figure-gengar-150x150.jpg", bio: "I am Tom, I am passionate about programming.")}
-    before { subject.save }
+  subject do
+    User.new(name: 'Ali',
+             photo: 'https://www.qttoyslondon.com/wp-content/uploads/2021/01/pokemon-45inch-battle-figure-gengar-150x150.jpg',
+             bio: 'I am Tom, I am passionate about programming.')
+  end
+  before { subject.save }
 
-    it 'name should be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should valid the name' do
+    subject.name = 'Rida'
+    expect(subject).to be_valid
+  end
+
+  describe 'Post counter' do
+    it 'should allow valid values' do
+      subject.posts_counter = 2.5
+      expect(subject).to_not be_valid
     end
 
-    it 'should valid the name' do
-        subject.name = 'Rida'
-        expect(subject).to be_valid
+    it 'should allow valid values' do
+      subject.posts_counter = -2
+      expect(subject).to_not be_valid
     end
 
-    describe 'Post counter' do
-
-        it 'should allow valid values' do
-            subject.posts_counter = 2.5
-            expect(subject).to_not be_valid
-        end
-
-        it 'should allow valid values' do
-            subject.posts_counter = -2
-            expect(subject).to_not be_valid
-        end
-
-        it 'should allow valid values' do
-            subject.posts_counter = 0
-            expect(subject).to be_valid
-        end
-
-        it 'should allow valid values' do
-            subject.posts_counter = 2
-            expect(subject).to be_valid
-        end
+    it 'should allow valid values' do
+      subject.posts_counter = 0
+      expect(subject).to be_valid
     end
 
-    describe 'validates recent_posts method' do
-        before {10.times { |i| Post.create(user:subject, title: 'post#{i}')}}
-
-        it 'should get last 5 comments' do
-            expect(subject.recent_posts).to eq(subject.posts.last(3))
-        end
+    it 'should allow valid values' do
+      subject.posts_counter = 2
+      expect(subject).to be_valid
     end
+  end
+
+  describe 'validates recent_posts method' do
+    before { 10.times { |a| Post.create(user: subject, title: "post#{a}") } }
+
+    it 'should get last 5 comments' do
+      expect(subject.recent_posts).to eq(subject.posts.last(3))
+    end
+  end
 end
