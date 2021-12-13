42a849eb0c7ce54473cfe0ce50ea6d296586d633
ridaarif98 <ridaarif16@gmail.com>
2021-12-13 13:44:50 +0100

    Setup devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # Removes one or more objects from the collection by setting their foreign keys to NULL.
   # Objects will be in addition destroyed if they're associated with dependent: :destroy
   has_many :posts, dependent: :destroy
