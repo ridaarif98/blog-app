f1e19266753a1be13d41a7c97b8a542afe3dca54
ridaarif98 <ridaarif16@gmail.com>
2021-12-15 16:23:30 +0100

    Fix request folder test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { User.create(name: 'Kolly', email: 'kolly@mail.com', password: 'password') }
+  let(:post) { user.posts.create(title: 'Post',text: 'Test', comments_counter: 2, likes_counter: 4) }
+  
   describe 'GET #index' do
-    before { get user_posts_path(1) }
+  before do
+    sign_in user
+    get user_posts_path(user_id: post.user_id)
+  end
     it 'should return response status correct (ok)' do
       expect(response).to have_http_status(:ok)
     end
@@ -11,27 +19,24 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('index')
     end
 
-    it 'response body includes correct placeholder text' do
-      expect(response.body).to include('Here is the list of all posts of a user')
-    end
-
     it 'respons to html' do
       expect(response.content_type).to include 'text/html'
     end
   end
 
   describe 'GET #show' do
-    before { get user_post_path(1, 2) }
+
+    before do
+      sign_in user
+      get user_post_path(user_id: user.id, id: post.id)
+    end
+
     it 'should return response status correct (ok)' do
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the show template' do
       expect(response).to render_template('show')
-    end
-
-    it 'response body includes correct placeholder text' do
-      expect(response.body).to include('Here is detail of a post')
     end
 
     it 'responds to html' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password') }
   describe 'GET #index' do
-    before { get users_path }
+  before do
+    sign_in user
+    get users_path
+  end
     it 'should return response status correct (ok)' do
       expect(response).to have_http_status(:ok)
     end
@@ -10,24 +16,19 @@ RSpec.describe 'Users', type: :request do
     it 'renders the index template' do
       expect(response).to render_template('index')
     end
-
-    it 'response body includes correct placeholder text' do
-      expect(response.body).to include('Here is the list of all users')
-    end
   end
 
   describe 'GET #show' do
-    before { get user_path(id: 1) }
+  before do
+    sign_in user
+    get user_path(user)
+  end
     it 'should return response status correct (ok)' do
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the show template' do
       expect(response).to render_template('show')
-    end
-
-    it 'response body includes correct placeholder text' do
-      expect(response.body).to include('Here is the profile of a selected user')
     end
 
     it 'responds to html by default' do
