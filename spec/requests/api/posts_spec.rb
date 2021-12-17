89eb346960f46184b3922e6ce8c76403b69960a7
ridaarif98 <ridaarif16@gmail.com>
2021-12-17 19:56:59 +0100

    Fix linter errors

--- spec/requests/api/posts_spec.rb
+++ spec/requests/api/posts_spec.rb
@@ -1,26 +1,25 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/posts', type: :request do
-    path 'api/v1/posts' do
-        get('list all posts') do
-            produces 'application/json'
-            consumes 'application/json'
-            response(200, 'Success') do
-                
-                schema type: :object,
-                properties: {
+  path 'api/v1/posts' do
+    get('list all posts') do
+      produces 'application/json'
+      consumes 'application/json'
+      response(200, 'Success') do
+        schema type: :object,
+               properties: {
                  id: { type: :integer },
                  title: { type: :string },
                  text: { type: :string }
                },
                required: %w[id title text]
-               let(:post) {Post.all}
-               run_test!
-            end
-            response '406', 'unsupported accept header' do
-                let(:Accept) { 'application/json' }
-                run_test!
-            end
-        end
+        let(:post) { Post.all }
+        run_test!
+      end
+      response '406', 'unsupported accept header' do
+        let(:Accept) { 'application/json' }
+        run_test!
+      end
     end
+  end
 end
